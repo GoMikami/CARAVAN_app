b82f3f99e6048fd5a281275e994932060378f64c
08g0531mkm@gmail.com <ec2-user@ip-172-31-85-55.ec2.internal>
2021-01-12 05:54:27 +0000

    [Finish]4-#

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,17 +1,19 @@
 class BlogsController < ApplicationController
-  
+
   def show
     @blog = Blog.find(params[:id])
   end
 
   def index
     @blogs = Blog.all
+    p "標準出力にのみ反映"
+    logger.debug("標準出力とログファイルに記録される")
   end
 
   def new
     @blog = Blog.new
   end
-  
+
   def create
     blog = Blog.new(blog_params)
     blog.save
@@ -21,13 +23,13 @@ class BlogsController < ApplicationController
   def edit
     @blog = Blog.find(params[:id])
   end
-  
+
   def update
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
   end
-  
+
   def destroy
     blog = Blog.find(params[:id])
     blog.destroy
@@ -38,5 +40,5 @@ class BlogsController < ApplicationController
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
   end
-  
+
 end
